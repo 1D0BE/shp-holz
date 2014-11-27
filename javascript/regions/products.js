5297e6eeb81e759a962bae64778d4f033a024bb6
1D0BE <matthias.hofreiter@gmail.com>
2014-11-27 16:13:56 +0100

    UNSTABLE: work-in-progress, button animation
    sidebar now sliding in and out

--- javascript/regions/products.js
+++ javascript/regions/products.js
@@ -1,9 +1,17 @@
 function clickMe(elem)
 {
+  console.log("Click");
+  var aside=$("aside");
+  console.log(aside);
   if((' ' + elem.className + ' ').indexOf("btn-selected") === -1) {
-    elem.className += " btn-selected";
+    elem.className += " btn-selected btnsided";
+    aside.addClass("sided");
+    console.log("if inside");
   }
   else {
     elem.className = elem.className.substring(0, elem.className.indexOf("btn-selected"))
+    aside.removeClass("sided");
+    console.log("else inside");
+
   }
 }
