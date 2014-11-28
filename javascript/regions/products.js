48a29dbff97d64a3d31269a341ef1e22544dd701
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-11-28 22:32:30 +0100

    remove log commands

--- javascript/regions/products.js
+++ javascript/regions/products.js
@@ -1,17 +1,12 @@
 function clickMe(elem)
 {
-  console.log("Click");
   var aside=$("aside");
-  console.log(aside);
   if((' ' + elem.className + ' ').indexOf("btn-selected") === -1) {
     elem.className += " btn-selected btnsided";
     aside.addClass("sided");
-    console.log("if inside");
   }
   else {
     elem.className = elem.className.substring(0, elem.className.indexOf("btn-selected"))
     aside.removeClass("sided");
-    console.log("else inside");
-
   }
 }
