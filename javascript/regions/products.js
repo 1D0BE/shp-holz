550449670f109cabd7f2038879e7ca9219600193
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-11-28 22:37:44 +0100

    gradient fix - now is "under" siderbar

--- javascript/regions/products.js
+++ javascript/regions/products.js
@@ -1,12 +1,15 @@
 function clickMe(elem)
 {
-  var aside=$("aside");
+  var aside=$("aside"),
+  body = $("body");
   if((' ' + elem.className + ' ').indexOf("btn-selected") === -1) {
     elem.className += " btn-selected btnsided";
     aside.addClass("sided");
+    body.removeClass("has-sidebar");
   }
   else {
     elem.className = elem.className.substring(0, elem.className.indexOf("btn-selected"))
     aside.removeClass("sided");
+    body.addClass("has-sidebar");
   }
 }
