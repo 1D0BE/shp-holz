b6452e115e1c2c9d2719c5e3f11f9157135a9c5e
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-03 20:11:01 +0100

    fixed clip js for window resize

--- javascript/nav.js
+++ javascript/nav.js
@@ -11,21 +11,37 @@ $(
     );
 
     var $mainImg = $('.main-section>article>img:not(.no-clip)'),
-    side,
-    actHeight;
-    $mainImg.css({
-      'height': 'auto',
-      'width': '50%',
-      'opacity': '0'
-      });
-      setTimeout(function () {
-        side = ($mainImg.height()-$mainImg.parent().parent().outerHeight());
-        actHeight = $mainImg.parent().parent().outerHeight() + side/2;
-        $mainImg.css({
-          'top': '-'+side/2+'px',
-          'clip': 'rect('+side/2+'px'+', 2000px,'+actHeight+'px'+', 0px)',
-          'opacity': '1'
-        });
-      }, 1);
+    imgRatio = $mainImg.height()/$mainImg.width();
+
+    function placeImg() {
+        var side,
+        actHeight,
+        imgHeight;
+        imgHeight = imgRatio*($(document).width()/2);
+            side = (imgHeight-$mainImg.parent().parent().outerHeight());
+            actHeight = $mainImg.parent().parent().outerHeight() + side/2;
+            if(imgHeight >= $mainImg.parent().parent().outerHeight()) {
+              $mainImg.css({
+                'height': 'auto',
+                'width': '50vw'
+                });
+              $mainImg.css({
+                'top': '-'+side/2+'px',
+                'clip': 'rect('+side/2+'px'+', 2000px,'+actHeight+'px'+', 0px)'
+              });
+            } else {
+              $mainImg.css({
+                'height': '100%',
+                'width': 'auto',
+                'clip': 'auto',
+                'top': 0
+                });
+            }
+    }
+
+    $(window).resize(function () {
+      placeImg();
+    });
+
   }
 );
