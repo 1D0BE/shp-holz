a116acc41d20af572478f17c6fed9ec7cf0401ae
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-03 20:46:40 +0100

    add img-load event handler to avoid infinity-bug

--- javascript/nav.js
+++ javascript/nav.js
@@ -11,36 +11,42 @@ $(
     );
 
     var $mainImg = $('.main-section>article>img:not(.no-clip)'),
-    imgRatio = $mainImg.height()/$mainImg.width();
+    imgRatio;
 
     function placeImg() {
         var side,
         actHeight,
         imgHeight;
         imgHeight = imgRatio*($(document).width()/2);
-            side = (imgHeight-$mainImg.parent().parent().outerHeight());
-            actHeight = $mainImg.parent().parent().outerHeight() + side/2;
-            if(imgHeight >= $mainImg.parent().parent().outerHeight() && !window.matchMedia('(max-width: 40.063em)').matches) {
-              $mainImg.css({
-                'height': 'auto',
-                'width': '50vw'
-                });
-              $mainImg.css({
-                'top': '-'+side/2+'px',
-                'clip': 'rect('+side/2+'px'+', 2000px,'+actHeight+'px'+', 0px)'
-              });
-            } else {
-              $mainImg.css({
-                'height': '',
-                'width': '',
-                'clip': '',
-                'top': ''
-                });
-            }
+        side = (imgHeight-$mainImg.parent().parent().outerHeight());
+        actHeight = $mainImg.parent().parent().outerHeight() + side/2;
+        if(imgHeight >= $mainImg.parent().parent().outerHeight() && !window.matchMedia('(max-width: 40.063em)').matches) {
+          if(side === 0) {
+            side = 1;
+          }
+          console.log('-'+side/2+'px'+$mainImg.width());
+          $mainImg.css({
+            'height': 'auto',
+            'width': '50vw',
+            'top': '-'+side/2+'px',
+            'clip': 'rect('+side/2+'px'+', 2000px,'+actHeight+'px'+', 0px)'
+          });
+        } else {
+          $mainImg.css({
+            'height': '',
+            'width': '',
+            'clip': '',
+            'top': ''
+            });
+        }
     }
 
-    $(window).resize(function () {
+    $mainImg.on('load', function () {
+      imgRatio = $mainImg.height()/$mainImg.width();
       placeImg();
+      $(window).resize(function () {
+        placeImg();
+      });
     });
 
   }
