11a51fcfe88b8ed168de288e8ead00b34b48998a
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-03 19:13:21 +0100

    add image clipping js

--- javascript/nav.js
+++ javascript/nav.js
@@ -9,5 +9,23 @@ $(
         }
       }
     );
+
+    var $mainImg = $('.main-section>article>img'),
+    side,
+    actHeight;
+    $mainImg.css({
+      'height': 'auto',
+      'width': '50%',
+      'opacity': '0'
+      });
+      setTimeout(function () {
+        side = ($mainImg.height()-$mainImg.parent().parent().outerHeight());
+        actHeight = $mainImg.parent().parent().outerHeight() + side/2;
+        $mainImg.css({
+          'top': '-'+side/2+'px',
+          'clip': 'rect('+side/2+'px'+', 2000px,'+actHeight+'px'+', 0px)',
+          'opacity': '1'
+        });
+      }, 1);
   }
 );
