630563a47248f4573a0c47d6d09ca591a774bbb6
Christoph Minixhofer <minchc11@htlkaindorf.at>
2014-12-11 21:38:05 +0100

    Change scrolling move styles to css

--- javascript/scrolling.js
+++ javascript/scrolling.js
@@ -6,28 +6,10 @@ $(
     lastScrollTop = 0,
     fontSize=$main.css("font-size");
 
-    $("body").css({
-      "height":"100%",
-      "overflow":"hidden"
-    });
+    $("body").addClass("scrolling");
 
     $(window).on("mousewheel", function(event){
-      if(firstTime) {
-          $(".main-section").css({
-            "width": "50%",
-            "float": "right"
-          });
-          $(".main-section p").css({
-            "left": "0",
-            "width": "100%"
-          });
-          $(".main-section img:not(.icon):not(.profile-image)").css({
-            "left": "-50vw"
-          });
-          firstTime = false;
-      }
       var st = $(this).scrollTop();
-      console.log(16*fontSize.replace("px",""));
       if (event.deltaY<0){
         TweenLite.to($main, 1, {top:$main.offset().top-(32*fontSize.replace("px","")+$main.height())});
       } else {
@@ -37,3 +19,11 @@ $(
     });
   }
 );
+
+function goUp($elem) {
+  TweenLite.to($elem, 1, {bottom:"100%"});
+}
+
+function goDown($elem) {
+  TweenLite.to($elem, 1, {top:"100%"});
+}
